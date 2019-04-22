cb7a15726afeab6365beaf474219a61151058523
Michał Filipiuk <mf385423@bruce.mimuw.edu.pl>
2019-04-22 22:24:07 +0200

    moving for the second time

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 def accuracy(network, validation_set_loader):
     correct = 0
@@ -13,3 +15,8 @@ def accuracy(network, validation_set_loader):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
     return correct/total
+
+def imshow(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
