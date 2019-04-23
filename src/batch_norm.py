27cfe33e4b9a6b6b53c9b80796321302b299277f
VAE (Michał Filipiuk) <mkfilipiuk@gmail.com>
2019-04-23 23:31:01 +0200

    forgot about batch_norm

--- src/batch_norm.py
+++ src/batch_norm.py
@@ -15,6 +15,6 @@ class Batch_norm_2d(nn.Module):
         a,b,c,d = x.shape
         x_transposed = x.transpose(0,1).contiguous().view(self.dimension, -1)
         mean = x_transposed.mean(1)
-        variance = x_transposed.var(1)
+        variance = torch.mean((x_transposed.view(self.dimension,-1) - mean.view(self.dimension,1)).pow(2),1)
         x_est = (x_transposed - mean.view(self.dimension,1)) / torch.sqrt(variance + self.eps).view(self.dimension, 1)
         return x_est.view(b,a,c,d).transpose(0,1) if self.affine is False else (x_est * self.gamma.view(self.dimension, 1) + self.beta.view(self.dimension, 1)).view(b,a,c,d).transpose(0,1)
